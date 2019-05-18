21325949e744105609ab7a904ce702dc3a8d2ddf
Granit Krasniqi <krasniqigranit59@gmail.com>
2019-05-18 23:51:06 +0200

    @EnableZuulProxy annotation added

--- src/main/java/com/karteladentare/kdapigateway/KdApigatewayApplication.java
+++ src/main/java/com/karteladentare/kdapigateway/KdApigatewayApplication.java
@@ -2,8 +2,10 @@ package com.karteladentare.kdapigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class KdApigatewayApplication {
 
     public static void main(String[] args) {
