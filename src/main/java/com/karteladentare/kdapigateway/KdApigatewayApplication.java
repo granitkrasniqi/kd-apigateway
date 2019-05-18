7771ba91b1ededdc525eadcdcc5754dd5df15164
Granit Krasniqi <krasniqigranit59@gmail.com>
2019-05-18 23:49:33 +0200

    first commit

---
+++ src/main/java/com/karteladentare/kdapigateway/KdApigatewayApplication.java
@@ -0,0 +1,13 @@
+package com.karteladentare.kdapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdApigatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KdApigatewayApplication.class, args);
+    }
+
+}
