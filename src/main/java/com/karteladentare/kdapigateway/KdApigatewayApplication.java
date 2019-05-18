001d234cfefb2a425cae331a4be7dff96675d450
Granit Krasniqi <krasniqigranit59@gmail.com>
2019-05-18 23:53:38 +0200

    Configure to communicate with service discovery

--- src/main/java/com/karteladentare/kdapigateway/KdApigatewayApplication.java
+++ src/main/java/com/karteladentare/kdapigateway/KdApigatewayApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableZuulProxy
+@EnableZuulProxy // Enable the service to be a Zuul server
 public class KdApigatewayApplication {
 
     public static void main(String[] args) {
